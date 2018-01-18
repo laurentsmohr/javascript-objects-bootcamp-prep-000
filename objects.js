e290d8d9ecde40e8079f12785f7b0b6fed0cd1e3
Laurents Mohr <laurents.mohr@whu.edu>
2018-01-18 14:30:38 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
 }
 
-function removeFromPlayList(playlist, artistName) {
+function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
 }
